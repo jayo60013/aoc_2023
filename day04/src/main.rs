1659ac7fa2e1c2dc32601cf78353a0b3fded521a
John Gatward <john.gatward@fdmgroup.com>
2023-12-05 22:25:24 +0000

    Cleaning up day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,35 +1,33 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() {
-    println!("Part 1: {}", part1("input"));
-    match part2("input") {
-        Ok(answer) => println!("Part 2: {}", answer),
-        Err(err) => eprintln!("Error reading file: {}", err),
-    }
+    print_result(part1("input"), "Part 1");
+    print_result(part2("input"), "Part 2");
 }
 
-fn part1(filename: &str) -> u32 {
-    if let Ok(contents) = fs::read_to_string(filename) {
-        return contents
-            .lines()
-            .filter_map(|line| {
-                let (winning, elf) = line.split_once(':').unwrap().1.split_once('|').unwrap();
-                let winning: HashSet<_> = winning.split_whitespace().collect();
-                let matches = elf
-                    .split_whitespace()
-                    .filter_map(|num| winning.contains(num).then_some(()))
-                    .count() as u32;
-                (matches > 0).then(|| 2u32.pow(matches - 1))
-            })
-            .sum();
-    } else {
-        eprintln!("Error reading file: {}", filename);
-        return 0;
+fn part1(filename: &str) -> io::Result<u32> {
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+    let mut total: u32 = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+        let (winning, elf) = line.split_once(':').unwrap().1.split_once('|').unwrap();
+        let winning: HashSet<_> = winning.split_whitespace().collect();
+        let matches = elf
+            .split_whitespace()
+            .filter_map(|num| winning.contains(num).then_some(()))
+            .count() as u32;
+        total += if matches > 0 {
+            2u32.pow(matches - 1)
+        } else {
+            0
+        };
     }
+    Ok(total)
 }
 
 fn part2(filename: &str) -> io::Result<u32> {
@@ -40,8 +38,8 @@ fn part2(filename: &str) -> io::Result<u32> {
 
     for (i, line) in reader.lines().enumerate() {
         copies.entry(i + 1).and_modify(|n| *n += 1).or_insert(1);
-
         let line = line?;
+
         let (winning, elf) = line.split_once(':').unwrap().1.split_once('|').unwrap();
         let winning: HashSet<_> = winning.split_whitespace().collect();
         let matches = elf
@@ -60,4 +58,11 @@ fn part2(filename: &str) -> io::Result<u32> {
     }
 
     Ok(copies.values().cloned().sum())
+}
+
+fn print_result(result: io::Result<u32>, part_name: &str) {
+    match result {
+        Ok(answer) => println!("{}: {}", part_name, answer),
+        Err(err) => eprintln!("Error reading file: {}", err),
+    }
 }
