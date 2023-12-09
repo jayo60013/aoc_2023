cac03d09b483c5f09f497eb4444214eddcfc459b
John Gatward <john.gatward@fdmgroup.com>
2023-12-09 00:53:34 +0000

    Adding day07 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -1,18 +1,9 @@
-CARD_ORDER = {
-    "A": 12,
-    "K": 11,
-    "Q": 10,
-    "J": 9,
-    "T": 8,
-    "9": 7,
-    "8": 6,
-    "7": 5,
-    "6": 4,
-    "5": 3,
-    "4": 2,
-    "3": 1,
-    "2": 0,
-}
+from collections import Counter
+
+PART1_ORDER = "23456789TJQKA"
+PART2_ORDER = "J23456789TQKA"
+
+CARD_ORDER = PART1_ORDER
 
 
 class Hand:
@@ -20,17 +11,17 @@ class Hand:
     bid: int
     hand: int
 
-    def __init__(self, cards, bid):
+    def __init__(self, cards, bid, hand):
         self.cards = cards
         self.bid = bid
-        self.hand = determineHand(cards)
+        self.hand = hand
 
     def __lt__(self, other) -> bool:
         if self.hand != other.hand:
             return self.hand < other.hand
         for lhs, rhs in zip(self.cards, other.cards):
             if lhs != rhs:
-                return CARD_ORDER[lhs] < CARD_ORDER[rhs]
+                return CARD_ORDER.index(lhs) < CARD_ORDER.index(rhs)
         return self.bid < other.bid
 
 
@@ -52,14 +43,45 @@ def determineHand(cards) -> int:
         return 0
 
 
-def part1(file) -> int:
+def adjustCard(cards) -> str:
+    if "J" not in cards:
+        return cards
+
+    card_counts = Counter(cards)
+    if card_counts["J"] == 5:
+        return "AAAAA"
+
+    del card_counts["J"]
+    most_common, _ = card_counts.most_common(1)[0]
+    return cards.replace("J", most_common)
+
+
+def part1(lines) -> int:
     hands = []
-    for line in file.readlines():
+    for line in lines:
         cards, bid = line.split(" ")
-        hands.append(Hand(cards, int(bid.strip())))
+        hands.append(Hand(cards, int(bid.strip()), determineHand(cards)))
     hands.sort()
     return sum((i + 1) * hand.bid for i, hand in enumerate(hands))
 
 
-file = open("input")
-print(f"Part 1: {part1(file)}")
+def part2(lines) -> int:
+    hands = []
+    for line in lines:
+        cards, bid = line.split(" ")
+        hands.append(
+            Hand(cards, int(bid.strip()), determineHand(adjustCard(cards)))
+        )
+
+    hands.sort()
+    return sum((i + 1) * hand.bid for i, hand in enumerate(hands))
+
+
+lines = []
+with open("input") as file:
+    for line in file:
+        lines.append(line)
+
+print(f"Part 1: {part1(lines)}")
+print(f"Part 2: {part2(lines)}")
+file.close()
