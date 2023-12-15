9803ec97b3f6219e8c1ff6cc3c6136561ee020f5
John Gatward <john.gatward@fdmgroup.com>
2023-12-15 22:27:36 +0000

    Adding day10 part1&2

--- day10/day10.py
+++ day10/day10.py
@@ -1,95 +1,157 @@
-import pdb
-
-DIRECTIONS = {
-    "up": [0, -1],
-    "down": [0, 1],
-    "left": [-1, 0],
-    "right": [1, 0],
-}
-WIDTH = 0
-HEIGHT = 0
-GRID = []
+from collections import deque
 
 
 def findStart(grid):
     for y, line in enumerate(grid):
         x = line.find("S")
         if x != -1:
-            return [x, y]
+            return (y, x)
 
 
-def checkDirection(dir, pipe) -> bool:
-    if dir == "up":
-        return pipe == "|" or pipe == "F" or pipe == "7"
-    elif dir == "down":
-        return pipe == "|" or pipe == "L" or pipe == "J"
-    elif dir == "left":
-        return pipe == "-" or pipe == "F" or pipe == "L"
-    elif dir == "right":
-        return pipe == "-" or pipe == "7" or pipe == "J"
-    else:
-        return False
+def part1(grid):
+    start = findStart(grid)
+    visited = {start}
+    toVisit = deque([start])
+
+    while len(toVisit) != 0:
+        row, col = toVisit.popleft()
+        sym = grid[row][col]
+
+        if (
+            row > 0
+            and sym in "S|JL"
+            and grid[row - 1][col] in "|7F"
+            and (row - 1, col) not in visited
+        ):
+            visited.add((row - 1, col))
+            toVisit.append((row - 1, col))
+
+        if (
+            row < len(grid) - 1
+            and sym in "S|7F"
+            and grid[row + 1][col] in "|JL"
+            and (row + 1, col) not in visited
+        ):
+            visited.add((row + 1, col))
+            toVisit.append((row + 1, col))
+
+        if (
+            col > 0
+            and sym in "S-J7"
+            and grid[row][col - 1] in "-LF"
+            and (row, col - 1) not in visited
+        ):
+            visited.add((row, col - 1))
+            toVisit.append((row, col - 1))
+
+        if (
+            col < len(grid[row]) - 1
+            and sym in "S-LF"
+            and grid[row][col + 1] in "-J7"
+            and (row, col + 1) not in visited
+        ):
+            visited.add((row, col + 1))
+            toVisit.append((row, col + 1))
+
+    return len(visited) // 2
 
 
-def checkBounds(x, y) -> bool:
-    return 0 <= x < WIDTH and 0 <= y < HEIGHT
+def removeNonLoopPipes(grid, visited):
+    new_grid = []
+    for r, row in enumerate(grid):
+        new_row = ""
+        for c, sym in enumerate(row):
+            if (r, c) in visited:
+                new_row += sym
+            else:
+                new_row += "."
+        new_grid.append(new_row)
+    return new_grid
 
 
-def startPipe(positions, lastVisited):
-    newPositions = []
+def part2(grid):
+    start = findStart(grid)
+    visited = {(start)}
+    toVisit = deque([(start)])
 
-    for pos, prev in zip(positions, lastVisited):
-        for dir, delta in DIRECTIONS.items():
-            x = pos[0] + delta[0]
-            y = pos[1] + delta[1]
-            if checkBounds(x, y) and checkDirection(dir, GRID[y][x]):
-                if [x, y] != prev:
-                    newPositions.append([x, y])
+    start_symbols = {"|", "-", "J", "L", "7", "F"}
 
-    return newPositions, positions
+    while toVisit:
+        row, col = toVisit.popleft()
+        sym = grid[row][col]
+
+        if (
+            row > 0
+            and sym in "S|JL"
+            and grid[row - 1][col] in "|7F"
+            and (row - 1, col) not in visited
+        ):
+            visited.add((row - 1, col))
+            toVisit.append((row - 1, col))
+            if sym == "S":
+                start_symbols &= {"|", "J", "L"}
+
+        if (
+            row < len(grid) - 1
+            and sym in "S|7F"
+            and grid[row + 1][col] in "|JL"
+            and (row + 1, col) not in visited
+        ):
+            visited.add((row + 1, col))
+            toVisit.append((row + 1, col))
+            if sym == "S":
+                start_symbols &= {"|", "7", "F"}
+
+        if (
+            col > 0
+            and sym in "S-J7"
+            and grid[row][col - 1] in "-LF"
+            and (row, col - 1) not in visited
+        ):
+            visited.add((row, col - 1))
+            toVisit.append((row, col - 1))
+            if sym == "S":
+                start_symbols &= {"-", "J", "7"}
+
+        if (
+            col < len(grid[row]) - 1
+            and sym in "S-LF"
+            and grid[row][col + 1] in "-J7"
+            and (row, col + 1) not in visited
+        ):
+            visited.add((row, col + 1))
+            toVisit.append((row, col + 1))
+            if sym == "S":
+                start_symbols &= {"-", "L", "F"}
+
+    if len(start_symbols) != 1:
+        print("ERR: Could not get start symbol")
+        exit(0)
+    (S,) = start_symbols
+    grid = [row.replace("S", S) for row in grid]
+    grid = removeNonLoopPipes(grid, visited)
+
+    outside = set()
+
+    for r, row in enumerate(grid):
+        within = False
+        up = None
+        for c, sym in enumerate(row):
+            if sym == "|":
+                within = not within
+            elif sym in "LF":
+                up = sym == "L"
+            elif sym in "7J":
+                if sym != ("J" if up else "7"):
+                    within = not within
+                up = None
+            if not within:
+                outside.add((r, c))
+
+    print(len(grid) * len(grid[0]) - len(outside | visited))
 
 
-def stepPipe(positions, lastVisited):
-    newPositions = []
-
-    for pos, prev in zip(positions, lastVisited):
-        for dir, delta in DIRECTIONS.items():
-            x = pos[0] + delta[0]
-            y = pos[1] + delta[1]
-            if checkBounds(x, y) and checkDirection(dir, GRID[y][x]):
-                if [x, y] != prev:
-                    newPositions.append([x, y])
-                    break
-
-    return newPositions, positions
-
-
-def part1():
-    positions = []
-    lastVisited = []
-    positions.append(findStart(GRID))
-    lastVisited = [positions, positions]
-
-    positions, lastVisited = startPipe(positions, lastVisited)
-    lastVisited = [positions, positions]
-
-    # pdb.set_trace()
-    count = 0
-    while positions[0] != positions[1]:
-        positions, lastVisited = stepPipe(positions, lastVisited)
-        print(positions)
-        count += 1
-
-    print(count)
-
-
-with open("sample.txt") as file:
-    for line in file:
-        GRID.append(line)
-WIDTH = len(GRID[0])
-HEIGHT = len(GRID)
-
-part1()
-# print(f"Part 1: {part1(lines)}")
+grid = open("input").read().strip().splitlines()
+# print(f"Part 1: {part1(grid)}")
+part2(grid)
 # print(f"Part 2: {part2(lines)}")
-file.close()
