8c40ed55e4740c5c3fc22627e41e5a33d38fc51f
John.Gatward <John.Gatward@fdmgroup.com>
2023-12-18 19:06:15 +0000

    Template for day14

--- day14/day14.go
+++ day14/day14.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	readFile, _ := os.Open("sample.txt")
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var input [][]byte
+
+	for fileScanner.Scan() {
+		input = append(input, []byte(fileScanner.Text()))
+	}
+
+	for c := 0; c < len(input); c++ {
+		for r := 0; r < len(input[0]); r++ {
+			fmt.Println("Suck your mum")
+		}
+	}
+
+	readFile.Close()
 }
